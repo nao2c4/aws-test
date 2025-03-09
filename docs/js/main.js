560a5ee5daed9b27cb826b99d20ab5a484b76552
nao2c4 <27849770+nao2c4@users.noreply.github.com>
2025-03-09 14:07:12 +0900

    :construction: 切り出す関数追加

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,14 +1,44 @@
+/**
+ *
+ * AWS サービス情報を管理するモジュール。
+ * このモジュールは AWS サービスデータを JSON ファイルから取得し、
+ * ランダムなサービス情報を提供する機能を実装しています。
+ *
+ * @module awsServiceModule
+ * @author nao2c4
+ * @version 1.0.0
+ * @description AWS サービス情報の読み込みと操作を行うモジュール
+ */
+
+// モジュールのコード...
 // AWS サービス情報を保持しておく変数。
 let awsServices = null;
 
 // AWS サービス情報の JSON ファイルのパス。
 const awsServicesJsonPath = "data/services.json";
 
-// AWS サービス情報を取得する関数。
-// fetch を使って JSON ファイルを取得し、取得した JSON データを返す。
+/**
+ * AWS サービスのデータを JSON ファイルから取得。
+ * @async
+ * @function getAwsServices
+ * @returns {Promise<Object>} AWS サービスデータオブジェクトを解決する Promise。
+ * @throws {Error} フェッチ操作が失敗した場合、または JSON パースが失敗した場合にエラーをスローします。
+ */
 const getAwsServices = async () => {
     const response = await fetch(awsServicesJsonPath);
     return await response.json();
+};
+
+
+/**
+ * 指定した数のランダムなAWSサービスを取得。
+ * @param {number} num - 取得するAWSサービスの数。
+ * @returns {Array} - ランダムに選択されたAWSサービスの配列。
+ * @description awsServices配列からランダムな連続したサービスを指定数だけ取得します。
+ */
+const getRandomAwsServices = (num) => {
+    const start = Math.floor(Math.random() * (awsServices.length - num + 1));
+    return awsServices.slice(start, start + num);
 };
 
 // スクリプトが読み込まれたら、AWS サービス情報を取得する。
